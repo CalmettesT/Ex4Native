80e33d90c49544e51f0afa231727272bf1f729c6
CalmettesT <78858536+CalmettesT@users.noreply.github.com>
2024-02-08 10:39:53 +0100

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import { Button, View, Image, Text, Platform, PermissionsAndroid } from 'react-native';
+import { launchImageLibrary, Asset, ImagePickerResponse } from 'react-native-image-picker';
+import { request, PERMISSIONS } from 'react-native-permissions';
+import { uploadImage } from './Client';
+
+
+const App = () => {
+  const [imageUri, setImageUri] = useState<string | null>(null);
+
+  const requestGalleryPermission = async () => {
+    // if (Platform.OS === 'android') {
+    //   const result = await request(PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE);
+    //   console.log(result);
+    //   if (result === PermissionsAndroid.RESULTS.GRANTED) { //PermissionsAndroid.RESULTS.GARANTED ne marche pas
+    //     console.log('Permission accordée');
+    //     selectImage();
+    //   } else {
+    //     console.log("Permission refusée, vous ne pouvez pas utiliser l'application sans accepter");
+    //   }
+    // }
+    selectImage();
+    }
+    
+    const selectImage = () => {
+      launchImageLibrary({ mediaType: 'photo' }, (response) => {
+        if (response.didCancel) {
+          console.log('User cancelled image picker');
+        } else if (response.errorMessage) {
+          console.log('ImagePicker Error: ', response.errorMessage);
+        } else if (response.assets && response.assets.length > 0) {
+          const uri = response.assets[0].uri;
+          if (uri) { // S'assure que `uri` n'est pas `undefined`
+            console.log('Image selected: ', uri);
+            setImageUri(uri); // `uri` est garanti d'être une string ici, jamais `undefined`
+          } else {
+            setImageUri(null); // Cas où `uri` est `undefined`
+            console.log("Image non sélectionné");
+          }
+        }
+      });
+    };
+    
+
+  const handleUploadImage = async () => {
+    if (imageUri) {
+      const response = await uploadImage(imageUri);
+      console.log(response);
+    }
+  };
+
+  return (
+    <View>
+      <Button title="Sélectionner une Image" onPress={requestGalleryPermission} />
+      {imageUri && <Image source={{ uri: imageUri }} />}
+      <Button title="Envoyer sur Imgur" onPress={handleUploadImage} />
+    </View>
+  );
+};
+
+export default App;
---
+++ Client.tsx
@@ -0,0 +1,30 @@
+const IMGUR_CLIENT_ID = '345cde336208da8';
+
+export const uploadImage = async (imageUri: string) => {
+  const formData = new FormData();
+  formData.append('image', {
+    uri: imageUri,
+    type: 'image/jpeg',
+    name: 'upload.jpg',
+  });
+
+  try {
+    const response = await fetch('https://api.imgur.com/3/image', {
+      method: 'POST',
+      headers: {
+        Authorization: `Client-ID ${IMGUR_CLIENT_ID}`,
+        'Content-Type': 'multipart/form-data',
+      },
+      body: formData,
+    });
+    if (response.ok) {
+      const result = await response.json();
+      console.log('Image uploaded to Imgur: ', result.data.link);
+      return result.data.link;
+    } else {
+      console.log('Upload failed');
+    }
+  } catch (error) {
+    console.error('Upload failed: ', error);
+  }
+};
